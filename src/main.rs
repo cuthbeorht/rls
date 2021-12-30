4762d2196133b34d81d0cf93068a5b9f96bdd964
David Sciacchettano <david.sciacchettano@gmail.com>
2021-12-30 10:26:56 -0500

    Get file properties completed

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,39 @@
 use std::io;
 use std::fs::{self, DirEntry};
 use std::path::Path;
+use std::env;
+
+struct File {
+    path: String,
+    name: String,
+    size_in_bytes: u32,
+    permissions: String,
+    is_file: bool
+}
 
 fn main() {
-    println!("Iterating over items in / directory");
+    let current_working_directory =  env::var("PWD").unwrap();
 
-    let mut entries = fs::read_dir("/").unwrap();
+    println!("Directory {}\n\n", current_working_directory);
+
+    let mut entries = fs::read_dir(current_working_directory).unwrap();
 
     for entry in entries {
-        println!("{}", entry.unwrap().path().display());
+        let file = entry.unwrap().path();
+        let file_metadata = fs::metadata(&file);
+        println!("{}", file.display());
+        println!("{} has the following metadata: {:?}", file.display(), file_metadata);
     }
+}
+
+fn get_file_info(full_name_and_path: String) -> File {
+    let file: File = File {
+        path: String::from("/home/davids/src/rls/src/main.rs"),
+        name: String::from("main.rs"),
+        size_in_bytes: 4096,
+        permissions: String::from("----------"),
+        is_file: true
+    };
+
+    return file
 }
