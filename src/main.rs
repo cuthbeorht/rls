51586e3b0bf53f5cc67d7bb6b9ffdf7762576939
David <cuthbeorht@users.noreply.github.com>
2022-01-08 11:26:24 -0500

    Add File Metadata (#11)

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,56 @@
 use std::io;
 use std::fs::{self, DirEntry};
 use std::path::Path;
+use std::path::PathBuf;
+use std::env;
+use std::os::unix::fs::PermissionsExt;
+use std::fs::Permissions;
+use std::fmt;
+
+struct File {
+    path: String,
+    name: String,
+    size_in_bytes: u64,
+    permissions: u32,
+    is_file: String
+}
 
 fn main() {
-    println!("Iterating over items in / directory");
+    let current_working_directory =  env::var("PWD").unwrap();
 
-    let mut entries = fs::read_dir("/").unwrap();
+    println!("Directory {}\n\n", current_working_directory);
+
+    let mut entries = fs::read_dir(current_working_directory).unwrap();
+
+    println!("File or Dir\tPermissions\tSize\tName");
 
     for entry in entries {
-        println!("{}", entry.unwrap().path().display());
+        let file = entry.unwrap();
+        let file_metadata = get_file_info(&file.path());
+        println!("{}\t\t{:?}\t\t{}\t{:?}", file_metadata.is_file, file_metadata.permissions, file_metadata.size_in_bytes, file_metadata.name);
     }
+}
+
+fn get_file_info(full_name_and_path: &PathBuf) -> File {
+
+    let file_metadata = fs::metadata(&full_name_and_path).unwrap();
+    let mut is_file: String = String::from("Dir");
+
+    if file_metadata.is_file() {
+        is_file = String::from("File")
+    }
+
+    let file_name_and_path = full_name_and_path.to_path_buf().as_path().display().to_string();
+    
+    let file_name = full_name_and_path.file_name().unwrap().to_str().unwrap().to_string();
+
+    let file: File = File {
+        path: file_name_and_path,
+        name: file_name,
+        size_in_bytes: file_metadata.len(),
+        permissions: file_metadata.permissions().mode(),
+        is_file: is_file
+    };
+
+    return file
 }
